bfea40032113d641748ebb30347baa36f6db2011
Seungbin Kim <seungbin.kim.dev@gmail.com>
2021-02-28 22:16:31 +0900

    docs: 주석 제거
    추후 구현예정으로 놓았던 주석을 제거

--- src/main/java/com/personal/board/entity/User.java
+++ src/main/java/com/personal/board/entity/User.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.Set;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -50,13 +49,5 @@ public class User extends BaseEntity {
 
   @Column(columnDefinition = "text", nullable = false)
   private String password;
-
-//  @ManyToMany
-//  @JoinTable(
-//      name = "user_authority",
-//      joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
-//      inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")}
-//  )
-//  private Set<Authority> authorities;
 
 }
