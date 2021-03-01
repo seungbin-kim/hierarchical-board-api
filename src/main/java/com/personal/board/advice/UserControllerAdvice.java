162da0c590c87b26b958a4c0e244276b9d8cc40b
Seungbin Kim <seungbin.kim.dev@gmail.com>
2021-03-01 20:25:37 +0900

    feat: 예외처리 추가, 에러응답 생성 메서드로 분리
    - NotFoundException 핸들러 추가
    - 에러응답 생성부분 메서드로 분리

--- src/main/java/com/personal/board/advice/UserControllerAdvice.java
+++ src/main/java/com/personal/board/advice/UserControllerAdvice.java
@@ -3,6 +3,8 @@ package com.personal.board.advice;
 import com.personal.board.controller.UserController;
 import com.personal.board.dto.response.ErrorResponse;
 import com.personal.board.exception.DuplicatedException;
+import com.personal.board.exception.NotFoundException;
+import com.personal.board.exception.PasswordIncorrectException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -15,11 +17,17 @@ import java.util.Objects;
 public class UserControllerAdvice {
 
   @ExceptionHandler(DuplicatedException.class)
-  public ResponseEntity<ErrorResponse> emailDuplicatedExceptionHandler(DuplicatedException exception) {
-    return
-        ResponseEntity
+  public ResponseEntity<ErrorResponse> duplicatedExceptionHandler(DuplicatedException exception) {
+    return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
-            .body(new ErrorResponse(ErrorType.BAD_ARGUMENT, exception.getMessage()));
+            .body(makeErrorResponse(ErrorType.BAD_ARGUMENT, exception.getMessage()));
+  }
+
+  @ExceptionHandler(NotFoundException.class)
+  public ResponseEntity<ErrorResponse> notFoundExceptionHandler(NotFoundException exception) {
+    return ResponseEntity
+        .status(HttpStatus.NOT_FOUND)
+        .body(makeErrorResponse(ErrorType.NOT_FOUND, exception.getMessage()));
   }
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -30,10 +38,20 @@ public class UserControllerAdvice {
             .getFieldError())
             .getDefaultMessage();
 
-    return
-        ResponseEntity
+    return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
             .body(new ErrorResponse(ErrorType.BAD_ARGUMENT, defaultMessage));
+  }
+
+  @ExceptionHandler(PasswordIncorrectException.class)
+  public ResponseEntity<ErrorResponse> passwordIncorrectExceptionHandler(PasswordIncorrectException exception) {
+    return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body(makeErrorResponse(ErrorType.BAD_ARGUMENT, exception.getMessage()));
+  }
+
+  private ErrorResponse makeErrorResponse(ErrorType errorType, String message) {
+    return new ErrorResponse(errorType, message);
   }
 
 }
