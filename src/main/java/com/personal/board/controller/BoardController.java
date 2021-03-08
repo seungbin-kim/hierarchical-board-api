5045e3a2fefdfefc32c4b0cf02a2aa711c1e009c
Seungbin Kim <seungbin.kim.dev@gmail.com>
2021-03-08 15:19:28 +0900

    feat: 기능 추가
    - 게시판에 게시물을 등록하고, 찾아오기 위한 기능 추가

--- src/main/java/com/personal/board/controller/BoardController.java
+++ src/main/java/com/personal/board/controller/BoardController.java
@@ -2,11 +2,12 @@ package com.personal.board.controller;
 
 import com.personal.board.dto.request.BoardRequest;
 import com.personal.board.dto.request.PostRequest;
-import com.personal.board.dto.response.board.BoardResponse;
 import com.personal.board.dto.response.board.BoardResponseWithCreatedAt;
 import com.personal.board.dto.response.board.BoardResponseWithDate;
 import com.personal.board.dto.response.ResultResponse;
-import com.personal.board.dto.response.post.PostResponseWithCreatedAt;
+import com.personal.board.dto.response.post.PostListResponse;
+import com.personal.board.dto.response.post.PostResponseWithContentAndCreatedAt;
+import com.personal.board.dto.response.post.PostResponseWithContentAndDate;
 import com.personal.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -22,10 +23,10 @@ import java.util.List;
 public class BoardController {
 
   private static final String BOARDS = "/api/v1/boards";
-  private static final String BOARD = BOARDS + "/{id}";
+  private static final String BOARD = BOARDS + "/{board-id}";
 
   private static final String POSTS = BOARD + "/posts";
-  private static final String POST = POSTS + "/{id}";
+  private static final String POST = POSTS + "/{post-id}";
 
   private final BoardService boardService;
 
@@ -43,20 +44,33 @@ public class BoardController {
         .ok(new ResultResponse<>(boardService.getAllBoard()));
   }
 
-  @GetMapping("/boards/{id}")
-  public ResponseEntity<BoardResponseWithDate> getBoard(@PathVariable final Long id) {
+  @GetMapping("/boards/{boardId}")
+  public ResponseEntity<BoardResponseWithDate> getBoard(@PathVariable final Long boardId) {
     return ResponseEntity
-        .ok(boardService.getBoard(id));
+        .ok(boardService.getBoard(boardId));
   }
 
-  @PostMapping("/boards/{id}/posts")
-  public ResponseEntity<PostResponseWithCreatedAt> uploadPost(
-      @RequestBody @Valid final PostRequest request, @PathVariable final Long id) {
-    PostResponseWithCreatedAt postResponse = boardService.addPost(request, id);
+  @PostMapping("/boards/{boardId}/posts")
+  public ResponseEntity<PostResponseWithContentAndCreatedAt> uploadPost(
+      @RequestBody @Valid final PostRequest request, @PathVariable final Long boardId) {
+    PostResponseWithContentAndCreatedAt postResponse = boardService.addPost(request, boardId);
     return ResponseEntity
         .created(new UriTemplate(POST)
-            .expand(id, postResponse.getId()))
+            .expand(boardId, postResponse.getId()))
         .body(postResponse);
+  }
+
+  @GetMapping("/boards/{boardId}/posts")
+  public ResponseEntity<ResultResponse<List<PostListResponse>>> getAllPost(@PathVariable final Long boardId) {
+    return ResponseEntity
+        .ok(new ResultResponse<>(boardService.getAllPost(boardId)));
+  }
+
+  @GetMapping("/boards/{boardId}/posts/{postId}")
+  public ResponseEntity<PostResponseWithContentAndDate> getPost(
+      @PathVariable final Long boardId, @PathVariable final Long postId) {
+    return ResponseEntity
+        .ok(boardService.getPost(boardId, postId));
   }
 
 }
