aedc5672fac5b4515b4559dfdee4fa1e72a913a7
Seungbin Kim <seungbin.kim.dev@gmail.com>
2021-03-22 18:45:37 +0900

    feat: jwt를 위한 설정
    - 권한 관련 엔티티 작성
    - User 엔티티 수정

---
+++ src/main/java/com/personal/board/entity/Authority.java
@@ -0,0 +1,22 @@
+package com.personal.board.entity;
+
+import com.personal.board.enumeration.Role;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Authority {
+
+  @Id
+  @Column(name = "authority_name", length = 50)
+  @Enumerated(EnumType.STRING)
+  private Role authorityName;
+
+}
--- src/main/java/com/personal/board/entity/User.java
+++ src/main/java/com/personal/board/entity/User.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -40,13 +42,28 @@ public class User extends BaseEntity {
   @Column(columnDefinition = "text", nullable = false)
   private String password;
 
-  public static User createUser(final String email, final String nickname, final String name, final LocalDate birthday, final String password) {
+  @ManyToMany
+  @JoinTable(
+      name = "user_authority",
+      joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
+      inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+  private Set<Authority> authorities = new HashSet<>();
+
+  public static User createUser(
+      final String email,
+      final String nickname,
+      final String name,
+      final LocalDate birthday,
+      final String password,
+      final Authority authority) {
+
     User user = new User();
     user.changeEmail(email);
     user.changeNickname(nickname);
     user.changeName(name);
     user.changeBirthday(birthday);
     user.changePassword(password);
+    user.authorities.add(authority);
     return user;
   }
 
