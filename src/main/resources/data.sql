09e82a9089f9060805b5a4b18085486244c68b35
Seungbin Kim <seungbin.kim.dev@gmail.com>
2021-04-06 06:09:23 +0900

    docs: add

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO authority
+VALUES ('ROLE_USER');
+INSERT INTO authority
+VALUES ('ROLE_ADMIN');
+
+INSERT INTO users(user_id, email, nickname, name, birthday, password, created_at, modified_at)
+VALUES (0, 'kim@a.b', '김아무개', '김', '1997-05-28', '$2a$10$6V.DbpQvKh79DqunMfPbkOdmd0xpjxIQwlgqslLdKA1.lmh7jWKSO',
+        current_timestamp, current_timestamp);
+
+INSERT INTO user_authority(authority_name, user_id) VALUES ('ROLE_ADMIN', 0);
