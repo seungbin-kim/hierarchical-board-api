20bb424b982a4a51d694af1f03503ebec9edff7b
Seungbin Kim <seungbin.kim.dev@gmail.com>
2021-04-20 11:59:18 +0900

    test: UserRepository 테스트
    - UserService 에서 사용하는 메서드 테스트

--- src/test/java/com/personal/board/repository/UserRepositoryUnitTest.java
+++ src/test/java/com/personal/board/repository/UserRepositoryUnitTest.java
@@ -1,17 +1,40 @@
 package com.personal.board.repository;
 
+import com.personal.board.entity.Authority;
+import com.personal.board.entity.User;
+import com.personal.board.enumeration.Role;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
 
 @AutoConfigureTestDatabase
 @DataJpaTest
 class UserRepositoryUnitTest {
 
   @Autowired
-  UserRepository userRepository;
+  private UserRepository userRepository;
+
+  String email = "test@test.com";
+  String name = "testName";
+  String nickname = "testNickname";
+  LocalDate birthday = LocalDate.parse("1997-05-28");
+  String password = "1234";
+  Authority authority = new Authority(Role.ROLE_USER);
+
+  @Test
+  void save() throws Exception {
+    //given
+    User user = User.createUser(email, nickname, name, birthday, password, authority);
+
+    //when
+    User savedUser = userRepository.save(user);
+
+    //then
+    Assertions.assertThat(savedUser).isEqualTo(user);
+  }
 
 }
