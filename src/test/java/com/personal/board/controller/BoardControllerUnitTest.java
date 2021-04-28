9d01c07e60a1024ff64deed3bb04471de5d568ba
Seungbin Kim <seungbin.kim.dev@gmail.com>
2021-04-28 10:46:17 +0900

    test: BoardController 테스트
    - Board 조회 단위테스트 추가

--- src/test/java/com/personal/board/controller/BoardControllerUnitTest.java
+++ src/test/java/com/personal/board/controller/BoardControllerUnitTest.java
@@ -3,12 +3,14 @@ package com.personal.board.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.personal.board.dto.request.BoardRequest;
 import com.personal.board.dto.response.board.BoardResponseWithCreatedAt;
+import com.personal.board.dto.response.board.BoardResponseWithDate;
 import com.personal.board.entity.Board;
 import com.personal.board.jwt.JwtAccessDeniedHandler;
 import com.personal.board.jwt.JwtAuthenticationEntryPoint;
 import com.personal.board.jwt.TokenProvider;
 import com.personal.board.service.BoardService;
 import com.personal.board.util.SecurityUtil;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -19,6 +21,11 @@ import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
@@ -46,11 +53,13 @@ class BoardControllerUnitTest {
   @MockBean
   private JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
+  String boardName = "testBoard";
+
   @Test
+  @DisplayName("게시판추가")
   @WithMockUser(roles = "ADMIN")
   void addBoard() throws Exception {
     //given
-    String boardName = "testBoard";
     BoardRequest boardRequest = new BoardRequest();
     boardRequest.setName(boardName);
     String content = new ObjectMapper()
@@ -75,6 +84,62 @@ class BoardControllerUnitTest {
         .andExpect(status().isCreated())
         .andExpect(jsonPath("$.id").value(1L))
         .andExpect(jsonPath("$.name").value(boardName))
+        .andDo(print());
+  }
+
+  @Test
+  @DisplayName("게시판목록조회")
+  void getAllBoard() throws Exception {
+    //given
+    List<Board> boardList = createBoard(10);
+    List<BoardResponseWithDate> responseWithDates = boardList.stream()
+        .map(BoardResponseWithDate::new)
+        .collect(Collectors.toList());
+
+    when(boardService.getAllBoard())
+        .thenReturn(responseWithDates);
+
+    //when
+    ResultActions perform = mockMvc.perform(get("/api/v1/boards")
+        .accept(MediaType.APPLICATION_JSON));
+
+    //then
+    perform
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.content", hasSize(10)))
+        .andDo(print());
+  }
+
+  private List<Board> createBoard(int number) {
+    List<Board> list = new ArrayList<>();
+    for (int i = 1; i <= number; i++) {
+      Board board = new Board("testBoard" + i);
+      ReflectionTestUtils.setField(board, "id", (long) i);
+      list.add(board);
+    }
+    return list;
+  }
+  
+  @Test
+  @DisplayName("게시판단건조회")
+  @WithMockUser(roles = "ADMIN")
+  void getBoard() throws Exception {
+    //given
+    Long id = 1L;
+    Board board = new Board(boardName);
+    ReflectionTestUtils.setField(board, "id", id);
+
+    when(boardService.getBoard(id))
+        .thenReturn(new BoardResponseWithDate(board));
+    
+    //when
+    ResultActions perform = mockMvc.perform(get("/api/v1/boards/{id}", id)
+        .accept(MediaType.APPLICATION_JSON));
+
+    //then
+    perform
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.id").value(id))
         .andDo(print());
   }
 
