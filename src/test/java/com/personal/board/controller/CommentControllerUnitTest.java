433f8201064647c56653d6daf49f82d94a063de5
Seungbin Kim <seungbin.kim.dev@gmail.com>
2021-05-12 16:38:21 +0900

    test: 테스트 작성
    - 댓글 페이지조회 테스트 작성

--- src/test/java/com/personal/board/controller/CommentControllerUnitTest.java
+++ src/test/java/com/personal/board/controller/CommentControllerUnitTest.java
@@ -1,6 +1,7 @@
 package com.personal.board.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.personal.board.dto.query.CommentQueryDto;
 import com.personal.board.dto.request.CommentRequest;
 import com.personal.board.dto.response.comment.CommentResponseWithCreatedAt;
 import com.personal.board.entity.*;
@@ -9,12 +10,14 @@ import com.personal.board.jwt.JwtAccessDeniedHandler;
 import com.personal.board.jwt.JwtAuthenticationEntryPoint;
 import com.personal.board.jwt.TokenProvider;
 import com.personal.board.service.CommentService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -22,6 +25,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -118,6 +124,94 @@ class CommentControllerUnitTest {
         .andExpect(jsonPath("$.id").value(commentId))
         .andExpect(jsonPath("$.content").value(content))
         .andDo(print());
+  }
+
+  @Test
+  @DisplayName("댓글페이지조회")
+  void getPageableComment() throws Exception {
+    //given
+    int page = 0;
+    int pageSize = 5;
+    PageRequest pageRequest = PageRequest.of(page, pageSize);
+
+    Long postId = 1L;
+    Long userId = 1L;
+    Long boardId = 1L;
+    Long commentId = 1L;
+
+    Board board = new Board("testBoard");
+    ReflectionTestUtils.setField(board, "id", boardId);
+
+    User user = User.createUser(
+        email,
+        nickname,
+        name,
+        birthday,
+        password,
+        authority
+    );
+    ReflectionTestUtils.setField(user, "id", userId);
+
+    Post post = Post.createPost(
+        board,
+        user,
+        "testTitle",
+        "testContent",
+        null
+    );
+    ReflectionTestUtils.setField(post, "id", postId);
+
+    String content = "test";
+    Comment comment = Comment.createComment(
+        post,
+        user,
+        content,
+        null
+    );
+    ReflectionTestUtils.setField(comment, "id", commentId);
+
+    int number = 5;
+    List<CommentQueryDto> parents = createCommentQueryDto(number, false);
+    PageImpl<CommentQueryDto> response = new PageImpl<>(parents, pageRequest, number);
+    when(commentService.getPageableComment(postId, pageRequest))
+        .thenReturn(response);
+
+    //when
+    ResultActions perform = mockMvc.perform(get("/api/v1/posts/{postId}/comments", postId)
+        .queryParam("page", String.valueOf(page))
+        .queryParam("size", String.valueOf(pageSize))
+        .accept(MediaType.APPLICATION_JSON));
+
+    //then
+    perform
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.content", Matchers.hasSize(number)))
+        .andExpect(jsonPath("$.content.[0].id").value(number))
+        .andExpect(jsonPath("$.content.[1].id").value(number - 1))
+        .andExpect(jsonPath("$.totalElements").value(number))
+        .andExpect(jsonPath("$.totalPages").value(1))
+        .andDo(print());
+  }
+
+  private List<CommentQueryDto> createCommentQueryDto(int number, boolean isChildren) {
+    List<CommentQueryDto> list = new ArrayList<>();
+    for (int i = number; i >= 1; i--) {
+      Long parentId = null;
+      long id = i;
+      if (isChildren) {
+        parentId = (long) i;
+        id = (i + number);
+      }
+      CommentQueryDto dto = new CommentQueryDto(
+          parentId,
+          id,
+          "nickname" + i,
+          "content" + i,
+          LocalDateTime.now(),
+          false);
+      list.add(dto);
+    }
+    return list;
   }
 
 }
